2f7fdc3fef8b4b7f017fbba45768ae2f1d4035a0
Amani Mettichi <amani.mettichi19@gmail.com>
2022-08-05 09:56:43 +0100

    first commit

---
+++ Probability.Test/AssertingProbabilityResults.cs
@@ -0,0 +1,31 @@
+﻿using Xunit;
+
+namespace Probability.Test
+{
+    public class AssertingProbabilityResults
+    {
+        [Fact]
+        public void ProbabilityOfA_And_B_Test()
+        {
+            var sut = new ProbabilityEvents(); // sut: system under test
+            var result = sut.ProbabilityOfA_And_B((decimal)0.35, (decimal)0.25);
+            Assert.Equal((decimal)0.0875, result);
+        }
+
+        [Fact]
+        public void ProbabilityOfA_Or_B_Test()
+        {
+            var sut = new ProbabilityEvents(); // sut: system under test
+            var result = sut.ProbabilityOfA_Or_B((decimal)0.35, (decimal)0.25);
+            Assert.Equal((decimal)0.5125, result);
+        }
+
+        [Fact]
+        public void ProbabilityInverseOfA_Test()
+        {
+            var sut = new ProbabilityEvents(); // sut: system under test
+            var result = sut.ProbabilityInverseOfA((decimal)0.35);
+            Assert.Equal((decimal)0.65, result);
+        }
+    }
+}
---
+++ Probability/ProbabilityEvents.cs
@@ -0,0 +1,24 @@
+﻿namespace Probability
+{
+    public struct ProbabilityEvents
+    {
+        public decimal ProbabilityOfA_And_B(decimal probabilityOfEventA, decimal probabilityOfEventB)
+        {
+            return probabilityOfEventA * probabilityOfEventB;
+        }
+
+        public decimal ProbabilityOfA_Or_B(decimal probabilityOfEventA, decimal probabilityOfEventB)
+        {
+            return probabilityOfEventA + probabilityOfEventB - ProbabilityOfA_And_B(probabilityOfEventA, probabilityOfEventB);
+        }
+
+        public decimal ProbabilityInverseOfA(decimal probabilityOfEventA)
+        {
+            return 1 - probabilityOfEventA;
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
